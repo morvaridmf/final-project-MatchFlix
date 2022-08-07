ebd6ed9a8f5db691c8b7ceffd958b64c34962ef0
Your Name <morvaridmahmoudi@gmail.com>
2022-08-07 16:31:54 +0200

    linting error gone

--- client/src/index.js
+++ client/src/index.js
@@ -34,7 +34,7 @@ export default function App() {
         setDislikedMovies([...data.disliked_movies])
       })
     }
-  }, [user])
+  }, [isAuthenticated])
 
   if(!isAuthenticated ){
     return <Login />
